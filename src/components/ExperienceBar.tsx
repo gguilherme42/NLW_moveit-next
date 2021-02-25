5098af2481ed59b818c33e94b7c9d3df07b82636
Guilherme Rosa <58306122+gguilherme42@users.noreply.github.com>
2021-02-25 00:52:55 -0300

    Adicionado o percentual para o próximo nível

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -6,12 +6,14 @@ import { ChallengesContext } from '../contexts/ChallengesContext';
 export function ExperienceBar() {
     const { currentExperience, experienceToNextLevel } = useContext(ChallengesContext);
 
+    const percentToNextLevel = (currentExperience * 100) / experienceToNextLevel;
+
     return (
         <header className={styles.experienceBar}>
             <span>0 xp</span>
             <div>
-                <div style={{width: '50%' }}></div>
-                <span className={styles.currentExperience} style={{ left: '50%' }}>{currentExperience} xp</span>
+                <div style={{width: percentToNextLevel}}></div>
+                <span className={styles.currentExperience} style={{ left: percentToNextLevel }}>{currentExperience} xp</span>
             </div>
             <span>{experienceToNextLevel} xp</span>
         </header>
