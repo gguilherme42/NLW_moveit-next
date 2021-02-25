2e6f6f02ef09e4adfc22111ae29323631e61c111
Guilherme Rosa <58306122+gguilherme42@users.noreply.github.com>
2021-02-24 23:12:18 -0300

    Componente colocado no index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ import { Profile } from '../components/Profile';
 import { CompletedChallenges } from '../components/CompletedChallenges';
 import { Countdown } from '../components/Countdown';
 import styles from '../styles/pages/Home.module.css';
+import { ChallengeBox } from '../components/ChallengeBox';
 
 
 export default function Home() {
@@ -23,7 +24,10 @@ export default function Home() {
             <CompletedChallenges />
             <Countdown />
           </div>
-          <div></div>
+          <div>
+            <ChallengeBox />
+
+          </div>
         </section>
     </div>
   )
