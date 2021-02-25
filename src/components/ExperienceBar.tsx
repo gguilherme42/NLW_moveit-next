b5608511197aa13477bef5510d84ce9642f9ad47
Guilherme Rosa <58306122+gguilherme42@users.noreply.github.com>
2021-02-25 00:55:45 -0300

    Arrdendamento adicionado para o cálculo do percentual para o próximo nível e interpolação para adicionar a porcentagem na largurar da div

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -6,14 +6,14 @@ import { ChallengesContext } from '../contexts/ChallengesContext';
 export function ExperienceBar() {
     const { currentExperience, experienceToNextLevel } = useContext(ChallengesContext);
 
-    const percentToNextLevel = (currentExperience * 100) / experienceToNextLevel;
+    const percentToNextLevel = Math.round((currentExperience * 100)) / experienceToNextLevel;
 
     return (
         <header className={styles.experienceBar}>
             <span>0 xp</span>
             <div>
-                <div style={{width: percentToNextLevel}}></div>
-                <span className={styles.currentExperience} style={{ left: percentToNextLevel }}>{currentExperience} xp</span>
+                <div style={{width: `${percentToNextLevel}%`}}></div>
+                <span className={styles.currentExperience} style={{ left: `${percentToNextLevel}%`}}>{currentExperience} xp</span>
             </div>
             <span>{experienceToNextLevel} xp</span>
         </header>
