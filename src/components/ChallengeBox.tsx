e60c1cdaa88c1f5caa3cd863dc8fc22765e59557
Guilherme Rosa <58306122+gguilherme42@users.noreply.github.com>
2021-02-24 23:06:57 -0300

    Início do componente ChallengeBox

---
+++ src/components/ChallengeBox.tsx
@@ -0,0 +1,10 @@
+import styles from '../styles/components/ChallengeBox.module.css';
+
+
+export function ChallengeBox() {
+    return (
+        <div className={styles.challengeBoxContainer}>
+            
+        </div>
+    );
+}
