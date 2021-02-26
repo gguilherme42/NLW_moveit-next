626cccfdbe981fcaf78b41e18021e3111ee81e6c
Guilherme Rosa <58306122+gguilherme42@users.noreply.github.com>
2021-02-26 15:23:36 -0300

    useContext no LevelUpModal para pegar o level do usuário. Correção de ortografia.

--- src/components/LevelUpModal.tsx
+++ src/components/LevelUpModal.tsx
@@ -1,12 +1,18 @@
+import { useContext } from 'react';
+
+import { ChallengesContext } from '../contexts/ChallengesContext';
+
 import styles from  '../styles/components/LevelUpModal.module.css';
 
 export function LevelUpModal() {
+    const { level } = useContext(ChallengesContext);
+
     return(
         <div className={styles.overlay}>
             <div className={styles.container}>
-                <header>2</header>
+                <header>{level}</header>
 
-                <strong>Parabéns</strong>
+                <strong>Parabéns!</strong>
                 <p>Você alcançou um novo level.</p>
 
                 <button type="button">
