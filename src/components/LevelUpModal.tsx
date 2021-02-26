3911ec30241da0bbc590249f8041d788cd820833
Guilherme Rosa <58306122+gguilherme42@users.noreply.github.com>
2021-02-26 15:30:44 -0300

    Adicionada função closeLevelUpModal para o botão de fechar o modal.

--- src/components/LevelUpModal.tsx
+++ src/components/LevelUpModal.tsx
@@ -5,7 +5,7 @@ import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from  '../styles/components/LevelUpModal.module.css';
 
 export function LevelUpModal() {
-    const { level } = useContext(ChallengesContext);
+    const { level, closeLevelUpModal } = useContext(ChallengesContext);
 
     return(
         <div className={styles.overlay}>
@@ -15,7 +15,7 @@ export function LevelUpModal() {
                 <strong>Parabéns!</strong>
                 <p>Você alcançou um novo level.</p>
 
-                <button type="button">
+                <button type="button" onClick={closeLevelUpModal}>
                     <img src="/icons/close.svg" alt="Fechar modal"/>
                 </button>
             </div>
