c5aefad5676321b8cdeb8967b0ab21751b3faf82
Guilherme Rosa <58306122+gguilherme42@users.noreply.github.com>
2021-02-26 14:51:15 -0300

    Adicionado level, currentExperience e challengesCompleted a interface IChallengesProviderProps. Uso do opertador ...rest nas props do ChallengesProvider. Uso das props novas na useState.

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -23,17 +23,21 @@ interface IChallengesContextData {
 
 interface IChallengesProviderProps {
     children: ReactNode;
+    level: number;
+    currentExperience: number;
+    challengesCompleted: number;
 };
 
 export const ChallengesContext = createContext({} as IChallengesContextData);
 
 
 
-export function ChallengesProvider({ children }: IChallengesProviderProps) {
+export function ChallengesProvider({ children, 
+    ...rest }: IChallengesProviderProps) {
 
-    const [level, setLevel] = useState(1);
-    const [currentExperience, setCurrentExperience] = useState(0);
-    const [challengesCompleted, setChallengesCompleted] = useState(0);
+    const [level, setLevel] = useState(rest.level ?? 1); // Se o level não existir, então, 1.
+    const [currentExperience, setCurrentExperience] = useState(rest.currentExperience ?? 0); 
+    const [challengesCompleted, setChallengesCompleted] = useState(rest.challengesCompleted ?? 0);
     const [activeChallenge, setActiveChallenge] = useState(null);
 
     const experienceToNextLevel = Math.pow((level + 1) * 4, 2);
